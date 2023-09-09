f94894fe834cc0735aee709b8981e354ff7a69f2
adamaho <aho.adam01@gmail.com>
2023-09-09 13:19:48 -0400

    (feat): adds echo framework example (#3)
    * (feat): adds echo-example
    
    * removing the benchmark code since its somewhat fud

---
+++ examples/echo/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/adamaho/realtime/pkg/realtime"
+	"github.com/labstack/echo/v4"
+)
+
+const SESSION_ID = "count"
+
+type data struct {
+	Count int `json:"count"`
+}
+
+func main() {
+	var count data = data{0}
+
+	e := echo.New()
+	rt := realtime.New()
+
+	e.GET("/count", func(c echo.Context) error {
+		json, _ := json.Marshal(count)
+		rt.Response(c.Response().Writer, c.Request(), json, SESSION_ID, true)
+		return nil
+	})
+
+	e.POST("/count/increment", func(c echo.Context) error {
+		count.Count += 1
+		json, _ := json.Marshal(count)
+		rt.SendMessage(json, SESSION_ID)
+		return c.String(http.StatusOK, "Count incremented.")
+	})
+
+	e.POST("/count/decrement", func(c echo.Context) error {
+		count.Count -= 1
+		json, _ := json.Marshal(count)
+		rt.SendMessage(json, SESSION_ID)
+		return c.String(http.StatusOK, "Count decremented.")
+	})
+
+	e.Logger.Fatal(e.StartTLS(":3000", "cert.pem", "key.pem"))
+}
