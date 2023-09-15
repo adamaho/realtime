549275f6d4661a4f2114ba5638c0fd7ed5d4447e
adamaho <aho.adam01@gmail.com>
2023-09-14 21:16:55 -0400

    (feat): adds new benchmark script

---
+++ scripts/benchmark/benchmark.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bufio"
+	"crypto/tls"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+const NUM_CLIENTS = 16
+
+func main() {
+	var wg sync.WaitGroup
+
+	for i := 0; i < NUM_CLIENTS; i++ {
+		wg.Add(1)
+
+		i := i
+		go func() {
+			defer wg.Done()
+
+			tls := &tls.Config{
+				InsecureSkipVerify: true,
+			}
+
+			client := &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig:   tls,
+					IdleConnTimeout:   60 * time.Second,
+					ForceAttemptHTTP2: true,
+				},
+			}
+
+			req, err := http.NewRequest("GET", "https://localhost:3000/count", nil)
+			if err != nil {
+				panic("Failed to get request for client")
+			}
+
+			req.Header.Set("x-stream", "true")
+
+			res, err := client.Do(req)
+			if err != nil {
+				fmt.Println("Oops, something went wrong with the request", err)
+			}
+
+			defer res.Body.Close()
+
+			reader := bufio.NewReader(res.Body)
+
+			for {
+				str, err := reader.ReadString('\n')
+				if err != nil {
+					return
+				}
+				fmt.Print(i, str)
+			}
+		}()
+	}
+
+	wg.Wait()
+}
